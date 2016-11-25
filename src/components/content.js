cf40cf2b1c7404c79deec39f023081c0bc55a8e4
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-11-25 14:44:25 +0100

    render examples

--- src/components/content.js
+++ src/components/content.js
@@ -4,7 +4,7 @@ const React = require('react')
 const Radium = require('radium')
 const {Style} = Radium
 const createUtils = require('../utils')
-const {sansSerifFont, monoFont} = require('./styles')
+const {sansSerifFont, monoFont, lineHeight, textColor} = require('./styles')
 
 const blockCodeStyle = {
   fontFamily: monoFont,
@@ -15,7 +15,43 @@ const blockCodeStyle = {
   marginBottom: '30px',
   position: 'relative',
   overflowX: 'auto',
-  fontSize: '13px'
+  fontSize: '13px',
+  color: textColor
+}
+
+const contentStyles = {
+  'h1, h2, h3, h4, h5, h6': {
+    fontFamily: sansSerifFont,
+    fontWeight: 'bold'
+  },
+  // Highlightjs Theme
+  '.hljs-meta': {
+    color: '#666'
+  },
+  '.hljs-comment': {
+    color: '#BBB'
+  },
+  '.hljs-number': {
+    color: '#9B80FF'
+  },
+  '.hljs-string': {
+    color: '#75DE00'
+  },
+  '.hljs-keyword, .hljs-function': {
+    color: '#F95FAA'
+  },
+  '.hljs-symbol, .hljs-class, .hljs-title, .hljs-literal': {
+    color: '#EEBA00'
+  },
+  '.hljs-params': {
+    color: '#444'
+  },
+  '.hljs-built_in': {
+    fontWeight: 'bold'
+  },
+  '.hljs-subst': {
+    color: textColor
+  }
 }
 
 const Signature = Radium(({member, utils}) => {
@@ -32,15 +68,61 @@ const Signature = Radium(({member, utils}) => {
   )
 })
 
+const Description = Radium(({content, utils}) => {
+  const rendered = {
+    __html: utils.md(content)
+  }
+
+  const style = {
+    marginTop: lineHeight(0.5),
+    marginBottom: lineHeight(0.5)
+  }
+
+  return (
+    <div
+      dangerouslySetInnerHTML={rendered}
+      style={style} />
+  )
+})
+
+const Example = Radium(({caption, content, utils}) => {
+  let renderedCaption
+
+  if (caption) {
+    renderedCaption = (
+      <p dangerouslySetInnerHTML={{__html: utils.md(caption)}} />
+    )
+  }
+
+  const rendered = {
+    __html: utils.highlight(content)
+  }
+
+  return (
+    <div>
+      <h4>Example</h4>
+      {renderedCaption}
+      <pre>
+        <code
+          style={blockCodeStyle}
+          dangerouslySetInnerHTML={rendered} />
+      </pre>
+    </div>
+  )
+})
+
 const SectionMember = Radium(({namespace, name, description, member, parent, utils}) => {
   return (
     <div>
       <h3>{parent}.{name}</h3>
       <Signature member={member} utils={utils} />
-      <div
-        dangerouslySetInnerHTML={{
-          __html: utils.md(description)
-        }} />
+      <Description content={description} utils={utils} />
+      {member.examples && member.examples.map((example) => (
+        <Example
+          name={example.caption}
+          content={example.description}
+          utils={utils} />
+       ))}
     </div>
   )
 })
@@ -64,8 +146,12 @@ const SectionGroup = Radium(({name, utils, members, parent}) => {
 
 const Section = Radium(({name, namespace, description, section, utils}) => {
   const members = section.members
-  const memberTypes = Object.keys(members).filter((key) => {
-    return members[key].length > 0
+  const memberTypes = [
+    'static',
+    'instance',
+    'events'
+  ].filter((key) => {
+    return members[key] && members[key].length > 0
   })
 
   return (
@@ -87,18 +173,11 @@ const Section = Radium(({name, namespace, description, section, utils}) => {
 })
 
 const Content = ({options, docs}) => {
-  const hStyle = {
-    fontFamily: sansSerifFont,
-    fontWeight: 'bold'
-  }
-
   const utils = createUtils(options, docs)
 
   return (
     <div>
-      <Style
-        scopeSelector='h1, h2, h3, h4, h5, h6'
-        rules={hStyle} />
+      <Style rules={contentStyles} />
       {docs.map((section) => (
         <Section
           key={section.name}
